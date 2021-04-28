0202c80ebaeecdb4d8a449ea289f20708db9add3
0wlRlyK <0wlrlyk@gmail.com>
2021-04-29 00:48:29 +0300

    Добавлено снаряжение пользователя

--- users/admin.py
+++ users/admin.py
@@ -3,41 +3,38 @@ from django.contrib.auth.admin import UserAdmin
 from django.db.utils import ProgrammingError
 from django.utils.translation import gettext_lazy as _
 
-from users.models import User, UserSettings, Award
+from users import models as users_models
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = User
+    model = users_models.User
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'avatar', 'ip', 'birthday', 'country',
                                          'state', 'signature', 'title', 'gender', 'rank', 'respect', 'banreason',
-                                         'money', 'ico_num', 'chat_color',
+                                         'money', 'ico_num', 'chat_color', "equip"
                                          )}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        ("Снаряга", {'fields': ('slot1', 'slot2', 'slot3', 'addon1', 'addon2', 'addon3', 'ammo1', 'ammo2',
-                                'upgrade1', 'upgrade2', 'upgrade3',
-                                )}),
     )
     list_display = ['username', 'id', 'email', "valid_username", ]
 
 
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(users_models.User)
+admin.site.register(users_models.User, CustomUserAdmin)
 
 
-@admin.register(Award)
+@admin.register(users_models.Award)
 class AwardAdmin(admin.ModelAdmin):
     list_display = ["name", "description", "id", "icon_admin"]
 
 
-@admin.register(UserSettings)
+@admin.register(users_models.UserSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
     # Створимо об'єкт за замовчуванням при першому сторінки SiteSettingsAdmin зі списком налаштувань
     def __init__(self, model, admin_site):
@@ -45,7 +42,7 @@ class SiteSettingsAdmin(admin.ModelAdmin):
         # обов'язково оберніть завантаження і збереження SiteSettings в try catch,
         # Щоб можна було виконати створення міграцій бази даних
         try:
-            UserSettings.load().save()
+            users_models.UserSettings.load().save()
         except ProgrammingError:
             pass
 
@@ -56,3 +53,53 @@ class SiteSettingsAdmin(admin.ModelAdmin):
     # а також видалення існуючих
     def has_delete_permission(self, request, obj=None):
         return False
+
+
+@admin.register(users_models.Weapon)
+class WeaponAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.Pistol)
+class PistolAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.Outfit)
+class OutfitAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.Ammo)
+class AmmoAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.PistolAmmo)
+class PistolAmmoAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.Grenade)
+class GrenadeAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.Addon)
+class AddonAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.UpgradeWeapon)
+class UpgradeWeaponAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.UpgradeOutfit)
+class UpgradeOutfitAdmin(admin.ModelAdmin):
+    list_display = ["name", "icon_admin", "cost"]
+
+
+@admin.register(users_models.Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ["id"]
--- users/models.py
+++ users/models.py
@@ -38,45 +38,6 @@ def upload_to_item(instance, filename):
     return '/'.join(['Equipment', str(instance.item_type), filename])
 
 
-class InventoryItem(models.Model):
-    WEAPON = 'WPN'
-    GUN = 'GUN'
-    ARMOR = 'ARM'
-    AMMUNITION_1 = 'AMMO1'
-    AMMUNITION_2 = 'AMMO2'
-    GRENADE = 'GRND'
-    ADDON = 'ADD'
-    WEAPON_UP = 'WUP'
-    GUN_UP = 'GUP'
-    ARMOR_UP = 'AUP'
-    INV_TYPES = [
-        (WEAPON, 'Оружие 1слот'),
-        (GUN, 'Оружие 2слот'),
-        (ARMOR, 'Броня'),
-        (AMMUNITION_1, 'Аммуниция 1слот'),
-        (AMMUNITION_2, 'Аммуниция 2слот'),
-        (GRENADE, 'Гранаты'),
-        (ADDON, 'Аддоны'),
-        (WEAPON_UP, 'Апгрейды 1слот'),
-        (GUN_UP, 'Апгрейды 2слот'),
-        (ARMOR_UP, 'Апгрейды брони'),
-    ]
-    name = models.CharField(max_length=200, verbose_name="Название")
-    description = RichTextField(verbose_name="Описание", blank=True)
-    image = models.ImageField(verbose_name="Иконка", upload_to=upload_to_item)
-    item_type = models.CharField(max_length=5, verbose_name="Тип снаряжения", choices=INV_TYPES)
-
-    def __str__(self):
-        return self.name
-
-
-class EquipItem(models.Model):
-    item = models.ForeignKey(InventoryItem, verbose_name="Объект", on_delete=models.SET_NULL, null=True)
-    condition = models.SmallIntegerField(default=100, verbose_name="Состояние")
-    quantity = models.SmallIntegerField(default=0, verbose_name="Колличество")
-    capacity = models.SmallIntegerField(default=0, verbose_name="Заряд")
-
-
 class User(AbstractUser):
     GENDER_CHOICE = [
         (1, "Man"),
@@ -98,17 +59,8 @@ class User(AbstractUser):
     ico_num = models.PositiveSmallIntegerField(default=1, verbose_name="Номер иконки")
     chat_color = models.CharField(max_length=10, verbose_name="Цвет", null=True, blank=True)
 
-    slot1 = models.ForeignKey(EquipItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uslot1")
-    slot2 = models.ForeignKey(EquipItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uslot2")
-    slot3 = models.ForeignKey(EquipItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uslot3")
-    addon1 = models.ForeignKey(EquipItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uaddon1")
-    addon2 = models.ForeignKey(EquipItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uaddon2")
-    addon3 = models.ForeignKey(EquipItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uaddon3")
-    ammo1 = models.ForeignKey(InventoryItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uammo1")
-    ammo2 = models.ForeignKey(InventoryItem, blank=True, null=True, on_delete=models.SET_NULL, related_name="uammo2")
-    upgrade1 = models.ManyToManyField(InventoryItem, blank=True, related_name="user_upgr1")
-    upgrade2 = models.ManyToManyField(InventoryItem, blank=True, related_name="user_upgr2")
-    upgrade3 = models.ManyToManyField(InventoryItem, blank=True, related_name="user_upgr3")
+    equip = models.OneToOneField("Inventory", null=True, verbose_name="Снаряжение пользователя",
+                                 on_delete=models.SET_NULL)
 
     def valid_username(self):
         return bool(re.match(r'^[\w.@+-]+\Z', self.username))
@@ -221,6 +173,16 @@ class User(AbstractUser):
     def get_awards_count(self):
         return self.awarded_user.count()
 
+    def get_equipment(self):
+        if self.equip_id:
+            return self.equip
+        else:
+            inv = Inventory(id=self.id)
+            inv.save()
+            self.equip_id = inv.id
+            super(User, self).save()
+        return inv
+
 
 class UserProfile(UserenaBaseProfile):
     user = models.OneToOneField(User,
@@ -252,7 +214,6 @@ class Reputation(models.Model):
         return {"ico": "bulb.svg",
                 "sign": "",
                 "text": "Уровень репутации не изменен"}
-
 
     def __str__(self):
         return self.subject
@@ -339,3 +300,197 @@ class UserAward(models.Model):
     class Meta:
         verbose_name = "Награда пользователя"
         verbose_name_plural = "Награды пользователя"
+
+
+class EquipItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=150, verbose_name="Название объекта")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    cost = models.PositiveSmallIntegerField(default=0, verbose_name="Стоимость", blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class Weapon(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Guns/", verbose_name="Иконка")
+    capacity = models.PositiveSmallIntegerField(default=0, verbose_name="Вместимость", blank=True)
+    damage = models.PositiveSmallIntegerField(default=0, verbose_name="Повреждение оружия", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" style="transform: rotate(90deg);padding: 0;margin: '
+                           '0; height: 100px"/></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Оружие 1 слота"
+
+
+class Pistol(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Guns/pistol/", verbose_name="Иконка")
+    capacity = models.PositiveSmallIntegerField(default=0, verbose_name="Вместимость", blank=True)
+    damage = models.PositiveSmallIntegerField(default=0, verbose_name="Повреждение оружия", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" style="transform: rotate(90deg);padding: 0;margin: '
+                           '0; height: 100px"/></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Оружие 2 слота"
+
+
+class Outfit(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/", verbose_name="Иконка")
+    armor = models.PositiveSmallIntegerField(default=0, verbose_name="Броня", blank=True)
+    protection = models.PositiveSmallIntegerField(default=0, verbose_name="Защита", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Броня"
+
+
+class Ammo(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/ammo/", verbose_name="Иконка", blank=True)
+    capacity = models.PositiveSmallIntegerField(default=0, verbose_name="Вместимость", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Боерипасы 1 слота"
+
+
+class PistolAmmo(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/pistol_ammo/", verbose_name="Иконка")
+    capacity = models.PositiveSmallIntegerField(default=0, verbose_name="Вместимость", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Боерипасы 2 слота"
+
+
+class Grenade(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/grenades/", verbose_name="Иконка", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Гранаты"
+
+
+class Addon(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/addons/", verbose_name="Иконка", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Аддоны"
+
+
+class UpgradeWeapon(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/upgrades_weap/", verbose_name="Иконка", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Апгрейды оружия"
+
+
+class UpgradeOutfit(EquipItem):
+    icon = models.ImageField(upload_to="post_st/inventory/Equipment/upgrades_equip/", verbose_name="Иконка", blank=True)
+
+    def icon_admin(self):
+        return format_html('<center><img href="{0}" src="{0}" /></center>'.format(self.icon.url))
+
+    icon_admin.allow_tags = True
+    icon_admin.short_description = 'Иконка'
+
+    class Meta:
+        verbose_name_plural = verbose_name = "Апгрейды брони"
+
+
+class Inventory(models.Model):
+    id = models.AutoField(primary_key=True)
+    slot1 = models.ForeignKey(Weapon, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Слот 1")
+    slot1_condition = models.PositiveSmallIntegerField(default=100, null=True, verbose_name="Состояние 1 слота")
+    slot2 = models.ForeignKey(Pistol, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Слот 2")
+    slot2_condition = models.PositiveSmallIntegerField(default=100, null=True, verbose_name="Состояние 2 слота")
+    slot3 = models.ForeignKey(Outfit, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Слот 3")
+    slot3_condition = models.PositiveSmallIntegerField(default=100, null=True, verbose_name="Состояние 3 слота")
+    ammo_slot1 = models.ForeignKey(Ammo, on_delete=models.SET_NULL, blank=True, null=True,
+                                   verbose_name="Боеприпасы 1 слота")
+    ammo_slot1_quantity = models.PositiveSmallIntegerField(default=0, null=True,
+                                                           verbose_name="Кол-во боеприпасов 1 слота")
+    ammo_slot2 = models.ForeignKey(PistolAmmo, on_delete=models.SET_NULL, blank=True, null=True,
+                                   verbose_name="Боеприпасы 2 слота")
+    ammo_slot2_quantity = models.PositiveSmallIntegerField(default=0, null=True,
+                                                           verbose_name="Кол-во боеприпасов 2 слота")
+    grenade = models.ForeignKey(Grenade, on_delete=models.SET_NULL, blank=True, null=True,
+                                verbose_name="Гранаты")
+    grenade_quantity = models.PositiveSmallIntegerField(default=0, null=True, verbose_name="Кол-во боеприпасов 1 слота")
+    addon_slot1 = models.ForeignKey(Addon, on_delete=models.SET_NULL, blank=True, null=True, related_name="wpn_addon",
+                                    verbose_name="Аддон 1 слота")
+    addon_slot2 = models.ForeignKey(Addon, on_delete=models.SET_NULL, blank=True, null=True, related_name="pst_addon",
+                                    verbose_name="Аддон 2 слота")
+    upgrades_slot1 = models.ManyToManyField(UpgradeWeapon, verbose_name="Апгрейды 1 слота", blank=True,
+                                            related_name="slot1_upgr")
+    upgrades_slot2 = models.ManyToManyField(UpgradeWeapon, verbose_name="Апгрейды 2 слота", blank=True,
+                                            related_name="slot2_upgr")
+    upgrades_slot3 = models.ManyToManyField(UpgradeOutfit, verbose_name="Апгрейды 3 слота", blank=True,
+                                            related_name="slot3_upgr")
+
+    @staticmethod
+    def condition_color(percents):
+        if percents >= 75:
+            return "st_green"
+        elif (percents < 75) and (percents >= 50):
+            return "st_yellow"
+        elif (percents < 50) and (percents >= 25):
+            return "st_orange"
+        elif (percents < 25) and (percents >= 0):
+            return "st_red"
+
+    def get_sl1c_class(self):
+        return self.condition_color(self.slot1_condition)
+
+    def get_sl2c_class(self):
+        return self.condition_color(self.slot2_condition)
+
+    def get_sl3c_class(self):
+        return self.condition_color(self.slot3_condition)
+
+    class Meta:
+        verbose_name = verbose_name_plural = "Снаряжение"
